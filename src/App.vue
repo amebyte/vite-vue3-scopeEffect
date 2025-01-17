4daf567f848ebb1dd285d3220890a0082a148606
Cobyte <iamcobyte@gmail.com>
2025-01-17 20:47:17 +0800

    counter

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Counter1 from './components/Counter1.vue'
+import Counter2 from './components/Counter2.vue'
 </script>
 
 <template>
   <Counter1 />
+  <Counter2 />
 </template>
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,18 +1,20 @@
 import { ref, watch } from "vue"
 
 export function useCounter() {
+    // 定义计数器
     const counter = ref(0)
-    
     // 增加
     const increment = () => counter.value++
     // 减少
     const decrement = () => counter.value--
-
+    // 计数器的偶数双倍值
     const doubleCount = ref(0)
-
+    // 监听计数器值的变化
     watch(() => counter.value, (newVal) => {
-        // 如果是 2 的倍数那么就将 counter 值乘以2
-        if (newVal && newVal % 2 === 0) {
+        console.log('newVal', newVal, newVal % 2 === 0)
+        // 当计数器的值是偶数时，计算并存储这个值的两倍
+        if (newVal % 2 === 0) {
+            console.log('xxx')
             doubleCount.value = newVal * 2 
         }
     })
